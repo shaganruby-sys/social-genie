351b4b35b40c34a73fbbd4f20deab738864ec794
shaganruby-sys <shaganruby@gmail.com>
2026-02-14 16:06:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import SocialGenie from "./SocialGenie";
+import dynamic from "next/dynamic";
+
+const SocialGenie = dynamic(() => import("./SocialGenie"), { ssr: false });
 
 export default function Page() {
   return <SocialGenie />;
